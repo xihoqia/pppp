dc296d60e5f45358f95b80f1b3d4bee7ef6e287d
houqiang <houqiang0219@gmail.com>
2022-08-16 21:45:23 +0800

    feat:init

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const rootEI = document.getElementById("root");
+rootEI.innerHTML=5555555
+const init =() => {
+    console.log(11111);
+}
+
+const init2 =() => {
+    console.log(33333);
+}
+
+init();
+
